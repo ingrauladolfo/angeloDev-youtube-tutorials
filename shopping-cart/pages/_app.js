c40e668b2a4d81de1036f88d63ff9e67dc876b4c
Raúl Torres Vargas <raultorresvargas@Rauls-MacBook-Air.local>
2022-09-18 20:05:23 -0500

    first commit

---
+++ shopping-cart/pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+export default MyApp
